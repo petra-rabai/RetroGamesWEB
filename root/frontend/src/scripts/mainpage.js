1d77218f66304e7676cc1a1030ba86344a1dbc6f
Petra Rábai <petra.rabai@icloud.com>
2022-05-13 17:31:00 +0200

    [in progress] - Loading screen implementation

---
+++ root/frontend/src/scripts/mainpage.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    var progressBarElement = document.getElementById('bar')
+    var progressBarValue = progressBarElement.aria-valuenow
+    function Loading(element) {
+      
+  }
+});
