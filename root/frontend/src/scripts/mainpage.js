325b0adfd90e6606d3366b513403aa99473bd237
Petra <petra.rabai@icloud.com>
2022-05-15 13:33:19 +0200

    [in progress] - Main page

--- root/frontend/src/scripts/mainpage.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    var progressBarElement = document.getElementById('bar')
-    var progressBarValue = progressBarElement.aria-valuenow
-    function Loading(element) {
-      
-  }
-});
